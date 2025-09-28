85a3644bfbf77c2f83fe6f1ffbb2dd72a3b8adf9
wliner <139436958+wlinerwr@users.noreply.github.com>
2025-09-28 21:46:25 +0800

    Add files via upload
    更新了对Tool的使用说明。短期记忆使用改用checkpointer实现

--- stock_agent.py
+++ stock_agent.py
@@ -7,6 +7,8 @@ from langchain_core.messages import HumanMessage
 from langgraph.graph import StateGraph,START,END
 from langgraph.prebuilt import ToolNode
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.prebuilt import create_react_agent
 
 from langchain_deepseek import ChatDeepSeek
 
@@ -27,11 +29,19 @@ class StockState(TypedDict):
     messages : Annotated[list,add_messages]
 
 
+
 #加载配置文件
 _ = load_dotenv(find_dotenv())
 deepseek_key = os.environ["deepseek_API_KEY"]
 
-message_history:list[BaseMessage] = []
+#使用短期内存处理同一线程多次对话的上下文
+checkpointer = InMemorySaver()
+checkpointer_config = {
+    "configurable": {
+        "thread_id" : "1"
+    }
+}
+
 
 #配置MCP服务器
 async def initialize_mcp_server() -> any:
@@ -46,7 +56,6 @@ async def initialize_mcp_server() -> any:
             }
         )
         tool = await client.get_tools()
-        #print(f"服务器可用工具：{tool}")
         return tool
     except Exception as e:
         print(f"服务器初始化错误    {e}")
@@ -61,6 +70,7 @@ def create_agent_graph(tool : list[BaseTool]) -> any:
             model="deepseek-chat",
             api_key=deepseek_key
         )
+        #绑定MCP服务器工具
         llm_with_tool  = llm.bind_tools(tools=tool)
     except:
         print("大模型初始化错误")
@@ -70,8 +80,11 @@ def create_agent_graph(tool : list[BaseTool]) -> any:
         """该用于与用户进行交互聊天"""
         #调用 DeepSeek 大模型生成回复
         #返回大模型回答结果，节点结束后，LangGraph内部自动处理状态合并
-        print(f"-------"*10 + "聊天节点调用" +"-------"*10)
-        print(state["messages"])
+        print("======"*10 + "聊天节点调用" + "======"*10)
+        print(f"-------当前状态信息------\n")
+        for messages in state["messages"]:
+            print(messages)
+        print("\n-------状态信息结束------\n")
         return{"messages":[llm_with_tool.invoke(state["messages"])]}
 
     try:
@@ -101,7 +114,9 @@ def create_agent_graph(tool : list[BaseTool]) -> any:
         }
     )
     graph.add_edge("tool","chat")
-    app = graph.compile()
+    #checkpointer,让LangGraph自动处理状态的保存和加载
+    app = graph.compile(checkpointer=checkpointer)
+
     # try:
     #     png_data =app.get_graph().draw_mermaid_png()
     #     with open("agent_graph.png", "wb") as f:
@@ -116,31 +131,33 @@ def create_agent_graph(tool : list[BaseTool]) -> any:
 
 
 #提交输入
-async def stream_graph_updates(user_input:HumanMessage ,app:any):
+async def stream_graph_updates(user_input:str ,app:any):
     #？？？？
     try:
-        async for event in app.astream({"messages": user_input}):
+        async for event in app.astream({"messages": user_input},checkpointer_config):
+            print("======"*10 + "节点调用结束" + "======="*10)
             #items()方法获得键，值
             for node_name,value in event.items():
                 ai_message = value["messages"][-1]
-                message_history.append(ai_message)
                 #检查value中是否有messages字段
                 if node_name == "chat" and "messages" in value:
                     if ai_message.tool_calls:
                         #需要tool节点
-                        print()
+                        print("======"*10 + "工具节点调用" + "======"*10)
                         for tool_call in ai_message.tool_calls:
-                            print(f"DeeSeek请求调用工具 : {tool_call['name']}")
-                            print(f" -参数 : {tool_call['args']}")
+                            print( "-" *10 + "DeeSeek请求调用工具" + "-" *10)
+                            print(f"工具名称 : {tool_call['name']}")
+                            print(f"传入服务器参数 : {tool_call['args']}")
+                            print("------------------------------------------")
                     else:
-                        print(f"DeepSeek: {ai_message.content}")
+                        print(f" - DeepSeek: {ai_message.content}")
                 if node_name == "tool" and "messages" in value:
                     for tool_messages in value["messages"]:
-                        print("----工具执行成----")
+                        print("====工具执行成====")
                         print(f"工具名称 : {tool_messages.name}")
-                        print("----------------------------------------")
+                        print("================================")
     except Exception as e:
-        print(f"输入错误:{e}")
+        print(f"提交数据错误:{e}")
 
 
 
@@ -152,12 +169,11 @@ async def main():
     print("="*50)
     while True:
         try:
-            user_input = input("用户:")
+            user_input = input(" - 用户:")
             if user_input.lower() in ["quit","exit","q"]:
                 print("再见")
                 break
-            message_history.append(HumanMessage(content=user_input))
-            #每次调用会创建全新messages列表，导致多次时交互messages只能记录一次交互的信息
+            #每次调用会创建新状态。
             await stream_graph_updates(user_input,app)
         except Exception as e:
             print("main错误 : " + e )
--- tushare_mcp_server.py
+++ tushare_mcp_server.py
@@ -3,6 +3,8 @@ import tushare
 import json
 import os
 import mcp
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv,find_dotenv
 from typing import TypedDict,Any
@@ -16,6 +18,9 @@ _ = load_dotenv(find_dotenv())
 
 token = os.environ["tushare_token1"]
 
+#创建代表北京时区对象
+shanghai_tz = ZoneInfo("Asia/Shanghai")
+
 ts = tushare.pro_api(token)
 
 
@@ -25,32 +30,36 @@ ts = tushare.pro_api(token)
 @mcp.tool()
 def get_stock_basic(exchange:str="",list_statues:str = "L",name:str="",limit:int="2",fields:str="ts_code,symbol,name,area,industry,list_date") -> str:
     """
-    调用工具可以获取获取上市股票基本信息，方法中有四个参数
+    调用工具可以获取获取上市股票基本信息.
+    参数输入说明:
     :param exchange 交易所, 可选值有:SSE上交所 SZSE深交所 BSE北交所,为空是全选,当用户未指定时使用默认值
     :param list_status: 上市状态, 可选值有： L上市 D退市 P暂停上市,当用户未指定时使用默认值
-    :param name:股票名字 传入的参入示例: "平安银行" ,为空是不指定.当用户未指定时使用默认值，当用户需要获取由股票名所指定股票的时，可以使用该参数获取股票信息。
+    :param name:股票名字 传入的参入示例: "平安银行" ,为空是不指定.当用户需要获取由股票名所指定股票的时，可以使用该参数获取股票信息。其他情况使用使用默认值
     :param limit : 获取返回数据条目的数目,当用户未指定时使用默认值,值最大不能超过10
     :param fields :规定返回的信息样式,可选字符串有:
-            ts_code:TS股票代码,是数据接口中的唯一标识。
-            symbol:股票代码.
-            name:股票简称.
-            area:公司注册的地域.
-            industry:所属行业.
-            fullname:股票全称.
-            enname:公司的英文全称。
-            cnspell:股票名称的中文拼音缩写.
-            market:市场类型.
-            exchange:交易所代码.
-            curr_type:交易货币.
-            list_status:上市状态。
-            list_date:上市日期.
-            delist_date:退市日期，
-            is_hs:是否为沪深港通标的。
-            act_name:实际控制人名称。
-            act_ent_type:实际控制人企业性质。
-            传入参数示例："ts_code,symbol,name,area,industry,list_date"
-            当用户没有明确指定返回的信息样式时使用默认的值，当默认的值没有用户需要的信息时在默认值后追加字符串，比如:
-            "ts_code,symbol,name,area,industry,list_date,delist_date"   在默认值后追加退市日期，
+        ts_code:TS股票代码,是数据接口中的唯一标识。
+        symbol:股票代码.
+        name:股票简称.
+        area:公司注册的地域.
+        industry:所属行业.
+        fullname:股票全称.
+        enname:公司的英文全称。
+        cnspell:股票名称的中文拼音缩写.
+        market:市场类型.
+        exchange:交易所代码.
+        curr_type:交易货币.
+        list_status:上市状态。
+        list_date:上市日期.
+        delist_date:退市日期，
+        is_hs:是否为沪深港通标的。
+        act_name:实际控制人名称。
+        act_ent_type:实际控制人企业性质。
+        传入参数示例："ts_code,symbol,name,area,industry,list_date"
+        当用户没有明确指定返回的信息样式时使用默认的值，当默认的值没有用户需要的信息时在默认值后追加字符串，比如:
+        "ts_code,symbol,name,area,industry,list_date,delist_date"   在默认值后追加退市日期.
+        返回结果说明:
+        1.结果正常
+        2.待定
     """
     try:
         respon = ts.stock_basic(exchange=exchange,limit=limit,name=name ,list_status=list_statues, fields=fields)
@@ -79,15 +88,42 @@ def get_stock_basic(exchange:str="",list_statues:str = "L",name:str="",limit:int
 
 
 @mcp.tool()
-def get_daily_data(ts_code:str,start_date:str,end_date:str):
+def get_daily_data(ts_code:str,trade_date:str,start_date:str="",end_date:str="",limit:int="2",fields:str="ts_code,trade_date,vol,amount"):
     """
     获取指定日期的历史日线
-    :param ts_code 标识股票代码
-    :param start_date 开始时间
+    参数输入说明:
+    :param ts_code 股票代码
+        说明:当ts_code未指定时(传入参数为空),会查寻规定日期中的所有股票相关的数据
+    :param trade_date:交易日期  
+    :param start_date 开始时间  
     :param end_date 结束时间
+        时间参数输入格式:YYYYMMDD格式 比如20180120
+        说明:若用户未指定时间,使用当天作为交易日期。开始时间与结束时间使用默认参数,查询当天的全部历史.
+        也可以使用该方式查询指定日期的历史日线
+    :param limit : 获取返回数据条目的数目,当用户未指定时使用默认值,值最大不能超过10
+    :param fields :规定返回的信息样式,可选字符串有:
+        ts_code:股票代码 
+        trade_date:交易日期
+        open:开盘价
+        high:最高价
+        low:最低价
+        close:收盘价
+        pre_close:昨收价
+        change:涨跌额
+        pct_chg:涨跌幅
+        vol:成交量 (单位:手) 
+        amount:成交额(单位：千元)
+        传入参数示例："ts_code,trade_date,vol,amount"
+        当用户没有明确指定返回的信息样式时使用默认的值，当默认的值没有用户需要的信息时在默认值后追加字符串，比如:
+        "ts_code,trade_date,vol,amount,open"   在默认值后追加开盘价.
+        返回结果说明:
+        1.结果正常
+        2.返回空值 ("[]"): 所查询的记录不存在.
+        3.待定
+
     """
     try:
-        respon = ts.daily(ts_code=ts_code,start_date=start_date,end_date=end_date)
+        respon = ts.daily(ts_code=ts_code,trade_date=trade_date,start_date=start_date,end_date=end_date,limit=limit,fields=fields)
         return respon.to_json(orient='records',force_ascii=False)
     except Exception as e:
         return json.dumps({"error" : str(e)})
@@ -101,7 +137,7 @@ def get_financial_indicator(ts_code:str,period:str="20250101"):
     """
     获取指定股票指定日期的财务指标数据
     :param ts_code:标识股票代码
-    :param period:指定的日期
+    :param period:指定日期
     """
     try:
         respon = ts.fina_indicator(ts_code=ts_code,period=period)
@@ -114,18 +150,56 @@ def get_financial_indicator(ts_code:str,period:str="20250101"):
 
 
 @mcp.tool()
-def get_stock_company(exchange:str,limit:int="10"):
+def get_stock_company(ts_code:str,exchange:str,limit:int="5",fields:str="ts_code,chairman,manager,secretary,reg_capital,setup_date,province"):
     """
     获取交易所中上市公司基础信息
-    :param exchange:交易所,SSE上交所 SZSE深交所 BSE北交所,为空是全选
+    :param ts_code:标识股票代码 精确查找时使用该参数,其他情况都为空
+    :param exchange:交易所,可选参数 SSE上交所 SZSE深交所 BSE北交所,为空是全选
     :param limit:返回数据条目的数目
+    :param fields :规定返回的信息样式,可选字符串有:
+        ts_code:股票代码
+        com_name:公司全称
+        com_id:统一社会信用代码
+        exchange:交易所代码
+        chairman:法人代表
+        manager:总经理
+        secretary:董秘
+        reg_capital:注册资本(万元)
+        setup_date:注册日期 (格式: 'YYYYMMDD')
+        province:所在省份
+        city:所在城市
+        introduction:公司介绍
+        website:公司主页
+        email:电子邮件
+        office:办公室(可能为空)
+        employees:员工人数
+        main_business:主要业务及产品(可能为空)
+        business_scope:经营范围(可能为空)
+        当用户没有明确指定返回的信息样式时使用默认的值，当默认的值没有用户需要的信息时在默认值后追加字符串，比如:
+        "s_code,chairman,manager,secretary,reg_capital,setup_date,province,website"   在默认值后追加开盘价.
+        返回结果说明:
+        1.结果正常(参数的返回值部分可能为空,代表这该公司暂时没有对应的信息。)
     """
     try:
-        respon = ts.stock_company(exchange=exchange,limit=limit)
+        respon = ts.stock_company(ts_code=ts_code,exchange=exchange,limit=limit)
         return respon.to_json(orient='records',force_ascii=False)
     except Exception as e:
         return json.dumps({"error" : str(e)})
 
+@mcp.tool()
+def get_current_time():
+    """
+    获取当前系统的时间
+    输入说明:
+    当用户未指定时区时使用北京时区,用户指定其他时区时使用北京时区，并说明只能返回北京时区的当前时间。
+    返回结果说明:
+    只返回北京时区的当前时间
+    示例:2023-09-20 10:27:21,表示2023年9月20日10时27分21秒
+    """
+    try:
+        return datetime.now(shanghai_tz)
+    except Exception as e:
+        return json.dumps({"error" : str(e)})
 
 
 
